4508834468d650e37145230a127baab3e616515b
Yupeng Han <yupenghan@MacBook-Pro-6.local>
2023-09-24 16:39:47 -0700

    camera pose

--- src/camera.hpp
+++ src/camera.hpp
@@ -15,6 +15,10 @@ class camera {
   int img_width = 100;
   int samples_per_pixel = 1;
   int max_reflection_depth = 10;
+  double vfov = 90;
+  point3 lookfrom = point3(0,0,-1); 
+  point3 lookat = point3(0,0,0);
+  point3 vup = point3(0,1,0);
 
   void render(const hittable& world) {
     initialize();
@@ -45,23 +49,37 @@ class camera {
     point3 pixel00_loc;
     vec3 pixel_delta_u;
     vec3 pixel_delta_v;
+    vec3   u, v, w;        // Camera frame basis vectors
   void initialize() {
     img_height = static_cast<int>(img_width / aspect_ratio);
     img_height = (img_height < 1 ) ? 1 : img_height;
 
-    camera_center = point3(0, 0, 0);
+    // camera_center = point3(0, 0, 0);
+    camera_center = lookfrom;
 
-    auto focal_length = 1.0;
-    auto viewport_height = 2.0;
+    // auto focal_length = 1.0;
+    auto focal_length = (lookfrom - lookat).length();
+    auto theta = degrees_to_radians(vfov);
+    auto h = tan(theta/2);
+    auto viewport_height = 2.0 * h * focal_length;
     auto viewport_width = viewport_height * (static_cast<double>(img_width)/img_height);
 
-    auto viewport_u = vec3(viewport_width, 0, 0);
-    auto viewport_v = vec3(0, -viewport_height, 0);
+    // Calculate the u,v,w unit basis vectors for the camera coordinate frame.
+    w = unit_vector(lookfrom - lookat);
+    u = unit_vector(cross(vup, w));
+    v = cross(w, u);
+
+    // auto viewport_u = vec3(viewport_width, 0, 0);
+    // auto viewport_v = vec3(0, -viewport_height, 0);
+    // Calculate the vectors across the horizontal and down the vertical viewport edges.
+    vec3 viewport_u = viewport_width * u;    // Vector across viewport horizontal edge
+    vec3 viewport_v = viewport_height * -v;  // Vector down viewport vertical edge
 
     pixel_delta_u = viewport_u / (img_width - 1);
     pixel_delta_v = viewport_v / (img_height - 1);
 
-    auto viewport_upper_left = camera_center - viewport_u/2 - viewport_v/2 - vec3(0, 0, focal_length);
+    // auto viewport_upper_left = camera_center - viewport_u/2 - viewport_v/2 - vec3(0, 0, focal_length);
+    auto viewport_upper_left = camera_center - (focal_length * w) - viewport_u/2 - viewport_v/2;
     pixel00_loc = viewport_upper_left;
   }
   ray get_ray(int i, int j) const {
--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main () {
   // World
 
   hittable_list world;
-  auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+  
   // auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
   // auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
   // auto material_right  = make_shared<metal>(color(0.99, 0.99, 0.99));
@@ -22,6 +22,23 @@ int main () {
   // auto material_center = make_shared<dielectric>(1.5);
   // auto material_left   = make_shared<dielectric>(1.5);
   // auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+  // auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
+  // auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+  // auto material_left   = make_shared<dielectric>(1.5);
+  // auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+
+  // world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+  // world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
+  // world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+  // world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   -0.4, material_left));
+  // world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+  // world.add(make_shared<sphere>(point3(0,0,-3), 2));
+  // world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
+  // world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
+  // world.add(make_shared<sphere>(point3(1,0,-1), 0.15));
+  // world.add(make_shared<sphere>(point3(-1,-0.1,-1), 0.35));
+  // world.add(make_shared<sphere>(point3(0,-100.5,-1), 100))
+  auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
   auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
   auto material_left   = make_shared<dielectric>(1.5);
   auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
@@ -29,20 +46,19 @@ int main () {
   world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
   world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
   world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-  world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   -0.4, material_left));
+  world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),  -0.4, material_left));
   world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
-  // world.add(make_shared<sphere>(point3(0,0,-3), 2));
-  // world.add(make_shared<sphere>(point3(0,0,-1), 0.5));
-  // world.add(make_shared<sphere>(point3(0,-100.5,-1), 100));
-  // world.add(make_shared<sphere>(point3(1,0,-1), 0.15));
-  // world.add(make_shared<sphere>(point3(-1,-0.1,-1), 0.35));
-  // world.add(make_shared<sphere>(point3(0,-100.5,-1), 100))
+
   camera cam;
 
   cam.aspect_ratio = 16.0 / 9.0;
   cam.img_width  = 400;
-  cam.samples_per_pixel = 500;
+  cam.samples_per_pixel = 100;
   cam.max_reflection_depth = 50;
+  cam.vfov = 20;
+  cam.lookfrom = point3(-2,2,1);
+  cam.lookat   = point3(0,0,-1);
+  cam.vup      = vec3(0,1,0);
 
   cam.render(world);
 }
